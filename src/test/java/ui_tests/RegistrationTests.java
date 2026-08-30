f6e978ebb5cba17109a4aa9a9a2fd094a5f31202
Olga Sheynina <sheynina82@gmail.com>
2026-08-30 19:01:01 +0300

    Start project

---
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -0,0 +1,43 @@
+package ui_tests;
+
+import dto.UserLombok;
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.util.Random;
+
+public class RegistrationTests extends AppManager {
+
+    @BeforeMethod
+    public void goToRegistrationLoginPage() {
+        new HomePage(getDriver()).clickBtnLogin();
+    }
+
+    @Test
+    public void registrationPositiveTest() {
+        int i = new Random().nextInt(1000);
+        UserLombok user = UserLombok.builder()
+                .username("gavana" + i + "55@club.com")
+                .password("Adfert55!")
+                .build();
+
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
