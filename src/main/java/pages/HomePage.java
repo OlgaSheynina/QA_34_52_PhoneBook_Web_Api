0551a168889d8611d0a23094ea6e6bb3790c5260
Olga Sheynina <sheynina82@gmail.com>
2026-09-02 19:30:57 +0300

    UserDataProvider

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,25 +1,47 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
+import static utils.PropertiesReader.*;
+
 public class HomePage extends BasePage {
 
     public HomePage(WebDriver driver) {
         setDriver(driver);
-        driver.get("https://telranedu.web.app/home");
+        //driver.get("https://telranedu.web.app/home");
+        driver.get(getProperty("base.properties", "baseUrl"));
         PageFactory.initElements(new AjaxElementLocatorFactory
                 (driver, 10), this);
     }
 
     @FindBy(xpath = "//a[text()='LOGIN']")
     WebElement btnLogin;
+    @FindBy(xpath = "//form/input[1]")
+    WebElement inputEmail;
 
     public void clickBtnLogin() {
         btnLogin.click();
+    }
+
+    public void method() {
+
+        WebElement login = driver.findElement
+                (By.xpath("//a[text()='LOGIN']"));
+        login.click();
+
+        WebElement inputEmail = driver.findElement
+                (By.xpath("//form/input[1]"));
+        inputEmail.sendKeys("shjgjgj@fgzfd.com");
+    }
+
+    public void ajaxMethod() {
+        btnLogin.click();
+        inputEmail.sendKeys("fshdfhdgj@dsfd.com");
     }
 }
 
--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -1,19 +1,28 @@
 package ui_tests;
 
+import data_providers.UserDataProvider;
 import dto.UserLombok;
 import manager.AppManager;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import pages.ContactsPage;
 import pages.HomePage;
 import pages.LoginPage;
+
+import static utils.UserFactory.*;
 
 import java.util.Random;
 
 public class RegistrationTests extends AppManager {
 
+    LoginPage loginPage;
+
     @BeforeMethod
     public void goToRegistrationLoginPage() {
         new HomePage(getDriver()).clickBtnLogin();
+        loginPage = new LoginPage(getDriver());
     }
 
     @Test
@@ -24,10 +33,80 @@ public class RegistrationTests extends AppManager {
                 .password("Adfert55!")
                 .build();
 
-        LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationForm(user);
         loginPage.clickBtnRegistration();
+
+        Assert.assertTrue(new ContactsPage(getDriver())
+                .validateTextInMessageNoContacts("No Contacts here!"));
     }
+
+    @Test
+    public void registrationPositiveWithFakerTest() {
+
+        UserLombok user = positiveUser();
+        System.out.println(user);
+
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+
+        Assert.assertTrue(new ContactsPage(getDriver())
+                .validateTextInMessageNoContacts("No Contacts here!"));
+    }
+
+    @Test
+    public void registrationNegativeEmptyAllFieldsTest() {
+
+        loginPage.clickBtnRegistration();
+
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+
+    @Test
+    public void registrationNegativeEmptyEmailFieldTest() {
+        UserLombok user = positiveUser();
+        user.setUsername("");
+
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+
+    @Test
+    public void registrationNegativeEmptyPasswordFieldTest() {
+        UserLombok user = positiveUser();
+        user.setPassword("");
+
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+
+    @Test(dataProvider = "dataProviderWrongPasswordOrEmail",
+            dataProviderClass = UserDataProvider.class)
+    public void registrationNegativeWrongPasswordTest(UserLombok user) {
+
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+
+        Assert.assertTrue(loginPage.closeAlert()
+                .contains("Wrong email or password format"));
+    }
+
+
+//    @Test
+//    public void testMethod() {
+//        new HomePage(getDriver()).method();
+//    }
+//
+//    @Test
+//    public void testAjaxMethod() {
+//        new HomePage(getDriver()).ajaxMethod();
+//    }
 }
 
 
